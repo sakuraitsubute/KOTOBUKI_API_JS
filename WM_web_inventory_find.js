f7b7443368bdffb3e6e05f4d2b9bb4fac6b3f57d
sakuraitsubute <65934184+sakuraitsubute@users.noreply.github.com>
2020-11-05 11:09:36 +0900

    Update WM_web_inventory_find.js

--- WM_web_inventory_find.js
+++ WM_web_inventory_find.js
@@ -86,32 +86,52 @@ $(function(){
 
 $(function(){
   $('#dicision_button').on('click', function(){
-    if(confirm('棚卸し結果を在庫に反映させてよろしいですか？（この操作は取り消せません）')){
-      $.ajax({
-        type:'POST',
-        url:'WM_web_inventory_dicision.php',
-        data:{
-          'code':'dicision'
+    $('#confirm').dialog({
+      modal:true,
+      title:"確認",
+      height:300,
+      position:{my: "center", at: "center", of: window},
+      buttons:{
+        "OK":function(){
+          $(this).dialog("close");
+          $.ajax({
+            type:'POST',
+            url:'WM_web_inventory_dicision.php',
+            data:{
+              'code':'dicision'
+            },
+            dataType:'JSON'
+          })
+          .done(function(data){
+            if(data.success){
+              $('#dicision').dialog({
+                modal:true,
+                title:"完了",
+                buttons:{
+                  "OK":function(){
+                    $(this).dialog("close");
+                    location.reload();
+                  }
+                }
+              })
+            }else if(data.fail){
+              alert(data.fail)
+            }else{
+              alert('データベースからの応答なし')
+              }
+            })
+          .fail(function(data){
+            alert('通信失敗');
+          })
         },
-        dataType:'JSON'
-      })
-      .done(function(data){
-        if(data.success){
-          console.log(data.success);
-          alert(data.success);
-        }else if(data.fail){
-          alert(data.fail)
-        }else{
-          alert('データベースからの応答なし')
-          }
-        })
-      .fail(function(data){
-        alert('通信失敗');
-      })
-      
-    }else{
-      alert('キャンセルしました')
-      return false;
+        "キャンセル":function(){
+          $(this).dialog("close");
+        }
       }
+    })
+    
+      
+      
+    
   })
 })
